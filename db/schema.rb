17c7708c23ac18a942a66c74f83bf05f61716d15
Narutoo9x <kingkid162@gmail.com>
2016-05-30 09:01:46 +0700

    rebuild

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160523024054) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string   "icon"
     t.string   "name"
@@ -54,8 +51,8 @@ ActiveRecord::Schema.define(version: 20160523024054) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "wallets", force: :cascade do |t|
     t.string   "name"
